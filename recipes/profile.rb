85b38956df46630b239f5904c4580d903d2620e1
Peter Rifel <pgrifel@gmail.com>
2017-01-30 23:10:37 -0800

    Add MOTD, bump version

--- recipes/profile.rb
+++ recipes/profile.rb
@@ -8,3 +8,17 @@ cookbook_file '/home/osmc/.bashrc' do
   group 'osmc'
   mode '0644'
 end
+
+cookbook_file '/home/osmc/.bash_profile' do
+  source 'bash_profile'
+  user 'osmc'
+  group 'osmc'
+  mode '0644'
+end
+
+file '/etc/motd' do
+  content ''
+  user 'root'
+  group 'root'
+  mode '0644'
+end
